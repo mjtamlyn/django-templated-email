3171c63d55c80f4a987b27b7c59f531f48c34d0c
Andrew Backer <awbacker@gmail.com>
2015-12-15 02:13:07 +0800

    Update templated backend for D1.9 by caching parent nodes

--- templated_email/utils.py
+++ templated_email/utils.py
@@ -7,9 +7,28 @@ try:
 except NameError:  # Python 3
     xrange = range
 
+node_parent_cache = {}
+
 
 class BlockNotFound(Exception):
     pass
+
+
+def get_node_parent(template_name, node, context):
+    """
+    Cache lookups for the parent node (the {% extends "..." %}).  In 1.9 the template will
+    be re-evaluated each time the node is hit (? no idea why). This wouldn't be an issue,
+    the second time the template loader can't find the file (why, did it lose the dirs?)
+
+     This is commonly a problem because of the call in vanilla_django where it extracts the
+     individual blocks.
+       for part in ['subject', 'html', ...]:
+    """
+    global node_parent_cache
+    parent = node_parent_cache.get(template_name, None)
+    if not parent:
+        parent = node_parent_cache[template_name] = node.get_parent(context)
+    return parent
 
 
 def _iter_nodes(template, context, name, block_lookups):
@@ -22,16 +41,23 @@ def _iter_nodes(template, context, name, block_lookups):
                     node.nodelist[i] = block_lookups[n.name]
             return node.render(context)
         elif isinstance(node, ExtendsNode):
+            # {% extends "email/my_base.email" %} or similar, e.g. this is a derived template
             lookups = dict([(n.name, n) for n in node.nodelist if isinstance(n, BlockNode)])
             lookups.update(block_lookups)
-            return _get_node(node.get_parent(context), context, name, lookups)
+            parent = get_node_parent(template.name, node, context)
+            return _get_node(parent, context, name, lookups)
 
     raise BlockNotFound("Node '%s' could not be found in template." % name)
 
 
-def _get_node(template, context=Context(), name='subject', block_lookups={}):
+def _get_node(template, context=Context(), name='subject', block_lookups=None):
+    if block_lookups is None:
+        block_lookups = {}
+
     try:
         return _iter_nodes(template, context, name, block_lookups)
     except TypeError:
+        # this is expected if `for node in template` failes.  this means the template is of type
+        # django.templates.backends.django.Template, so get the inner template which is the proper one
         context.template = template.template
         return _iter_nodes(template.template, context, name, block_lookups)
